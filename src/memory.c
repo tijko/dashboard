08419e9930d621a73c02f3e9d082773a252bf9b0
Tim Konick <konick781@gmail.com>
2014-08-05 17:43:27 -0400

    pass a char *

--- src/memory.c
+++ src/memory.c
@@ -4,15 +4,17 @@
 #include <stdlib.h>
 
 #include "memory.h"
-#include "util/procparse.h"
+#include "util/parser.h"
 
 
 int total_memory(void)
 {
     char *mempath = "/proc/meminfo";
-    void *memory = proc_parser(mempath, "MemTotal");
-    if (memory)
-        return *(int *) memory;
+    char *memory = proc_parser(mempath, "MemTotal");
+    if (memory != NULL) {
+        int value = strtol(memory, NULL, 10);
+        return value;
+    }
     return -1;
 }
 
@@ -21,8 +23,8 @@ void memory_percentage(proc_t *procs, int totalmem)
     float total_usage = 0;
     char *path = malloc(sizeof(char) * 256);
     snprintf(path, 256, "/proc/%d/status", procs->pid);
-    void *percentage = proc_parser(path, "VmSize");
-    if (percentage)
-        total_usage = *(float *) percentage;
+    char *percentage = proc_parser(path, "VmSize");
+    if (percentage != NULL)
+        total_usage = (float) strtol(percentage, NULL, 10);
     procs->mempcent = (total_usage / totalmem) * 100;
 }
