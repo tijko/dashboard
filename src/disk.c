fbf72d09d0e66bc1dbda6e629011c04c8e4cb9f5
Tim Konick <konick781@gmail.com>
2014-08-06 18:20:42 -0400

    call setfsent to set position to start of /etc/fstab

--- src/disk.c
+++ src/disk.c
@@ -4,7 +4,14 @@
 
 char *filesystem_type(void)
 {
+    int ret;
+
+    ret = setfsent();
+    if (ret != 1)
+        return NULL;
+
     struct fstab *file_system = getfsent();
+    endfsent();
     if (!file_system)
         return NULL;
     return file_system->fs_vfstype;
