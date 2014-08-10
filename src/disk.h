560e54d00f74ccc4b3851f06cc2ce21228300c64
Tim Konick <konick781@gmail.com>
2014-08-09 22:14:50 -0400

    ioprio macros

--- src/disk.h
+++ src/disk.h
@@ -1,1 +1,13 @@
+#define _GNU_SOURCE
+
+#ifdef __i386__
+#define IOPRIO_GET 290
+#elif __x86_64__
+#define IOPRIO_GET 252
+#endif
+
+#define IOPRIO_WHO_PROCESS 1
+
 char *filesystem_type(void);
+
+int ioprio_get(int pid);
