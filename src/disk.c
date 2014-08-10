dcd1513664573368a5a8aef8d442207f686e3a6e
Tim Konick <konick781@gmail.com>
2014-08-10 14:54:20 -0400

    ioprio_class function

--- src/disk.c
+++ src/disk.c
@@ -1,8 +1,11 @@
 #include <fstab.h>
+#include <sched.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <ncurses.h>
 #include <sys/syscall.h>
 
+#include "cpu.h"
 #include "disk.h"
 
 
@@ -26,4 +29,34 @@ int ioprio_get(int pid)
     long ioprio;
     ioprio = syscall(IOPRIO_GET, IOPRIO_WHO_PROCESS, pid);
     return ioprio;
+}
+
+char *ioprio_class(int pid)
+{
+    int ioprio;
+    char *class;
+
+    char *ioprio_classes[4] = {"", "Rt", "Be", "Id"};
+    
+    ioprio = ioprio_get(pid);
+    ioprio >>= IOPRIO_SHIFT;
+    if (ioprio != 0) {
+        class = ioprio_classes[ioprio];
+    } else {
+        int niceness;
+        int ioprio_level;
+
+        class = malloc(sizeof(char) * PRIOLEN);
+        ioprio = sched_getscheduler(pid);
+        niceness = nice(pid);
+        ioprio_level = (niceness + 20) / 5;
+        
+        if (ioprio == SCHED_FIFO || ioprio == SCHED_RR)
+            snprintf(class, PRIOLEN, "%s/%d", ioprio_classes[1], ioprio_level);
+        else if (ioprio == SCHED_OTHER)
+            snprintf(class, PRIOLEN, "%s/%d", ioprio_classes[2], ioprio_level);
+        else
+            class = ioprio_classes[3];
+    }
+    return class;
 }
