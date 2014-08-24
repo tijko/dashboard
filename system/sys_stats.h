771e87e8c50eede4bb62263b71c7674a3b7383cf
Tim Konick <konick781@gmail.com>
2014-08-23 22:38:14 -0400

    use macro for sizes

--- system/sys_stats.h
+++ system/sys_stats.h
@@ -1,6 +1,17 @@
 #ifndef SYSSTATS_H
 #define SYSSTATS_H
 
+#define MAXTOT 16
+
+#define BASE 1024
+
+#define MEMINFO "/proc/meminfo"
+
+#define MEMFREE "MemFree"
+
+#define SECS 60
+
+
 void build_info(char *fstype);
 
 char *mem_avail(unsigned long memory, unsigned long base);
