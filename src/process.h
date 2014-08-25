44aaa7f645b75c3a38830e60f56215cb2038ee2a
Tim Konick <konick781@gmail.com>
2014-08-24 23:04:42 -0400

    get field func. prototype

--- src/process.h
+++ src/process.h
@@ -2,9 +2,17 @@
 #define PROC_H
 
 #define PROCNAME_MAX 1024
+
 #define MAXPROCPATH 2048
+
 #define PROC "/proc"
+
 #define COMMLEN 0x20 
+
+#define UID "Uid"
+#define VMEM "VmSize"
+#define PTE "VmPTE"
+#define RSS "VmRSS"
 
 typedef struct process_attr proc_t;
 
@@ -13,12 +21,15 @@ struct process_attr {
     char *pidstr;
     char *user;
     int pid;
+    int uid;
     int cpuset;
     int nice;
     char *ioprio;
     char *state;
     float mempcent;
     int vmem;
+    int pte;
+    int rss;
     proc_t *prev;
     proc_t *next;
 };
@@ -31,9 +42,7 @@ int is_pid(char *process_name);
 
 void proc_user(proc_t *proc);
 
-int get_uid(char *pid);
-
-int get_vmem(char *pid);
+int get_field(char *pid, char *field);
 
 void name_pid(proc_t *procs);
 
