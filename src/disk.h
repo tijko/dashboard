99ff0de8a9c41ecbf5abf2a2a5c335097aed3b91
Tim Konick <konick781@gmail.com>
2015-01-02 23:18:34 -0500

    definitions for build_req

--- src/disk.h
+++ src/disk.h
@@ -15,6 +15,7 @@
 #define GENLMSG_PAYLOAD(nlh) (NLMSG_PAYLOAD(nlh, 0) - GENL_HDRLEN)
 #define NLA_PAYLOAD(nlh) (GENLMSG_PAYLOAD(nlh))
 #define NLA_DATA(nla) ((void *) ((char *) nla + NLA_HDRLEN))
+
 #define IOPRIO_WHO_PROCESS 1
 
 #define MAX_MSG_SZ 1024
@@ -29,6 +30,8 @@ struct nl_msg {
     char buf[MAX_MSG_SZ];
 };
 
+#define GET_REQUEST_LENGTH(msg) (((struct nl_msg *) msg)->nlh.nlmsg_len)
+
 char *filesystem_type(void);
 
 int ioprio_get(int pid);
@@ -41,8 +44,10 @@ int create_conn(void);
 
 int get_family_id(int conn);
 
-int nl_req(int conn, uint32_t nl_type, uint32_t gnl_cmd,
-    uint16_t nla_type, void *nla_data, uint16_t nla_len);
+char *build_req(uint32_t nl_type, uint8_t gnl_cmd, uint16_t nla_type, 
+                                    void *nla_data, uint16_t nla_len);
+
+int nl_req(int conn, char *buf, int buflen);
 
 bool nl_recv(int conn, struct nl_msg *req);
 
