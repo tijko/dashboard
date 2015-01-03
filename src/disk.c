2a061285720c19b93bc6eb6fa24407b1dd92b58f
Tim Konick <konick781@gmail.com>
2015-01-02 23:18:18 -0500

    split nl_req into build_req; caught wrong type in genl_cmd

--- src/disk.c
+++ src/disk.c
@@ -42,7 +42,6 @@ char *ioprio_class(int pid)
     int ioprio;
     char *class;
 
-    
     ioprio = ioprio_get(pid);
     ioprio >>= IOPRIO_SHIFT;
     if (ioprio != 0) { 
@@ -51,14 +50,13 @@ char *ioprio_class(int pid)
     } else {
         class = ioprio_class_nice(pid);
     }
+
     return class;
 }
 
 char *ioprio_class_nice(int pid)
 {
-    int niceness;
-    int ioprio;
-    int ioprio_level;
+    int niceness, ioprio, ioprio_level;
     char *class;
 
     class = malloc(sizeof(char) * PRIOLEN);
@@ -97,8 +95,8 @@ int create_conn(void)
 
 int get_family_id(int conn)
 {
-    int req, family_id, msgleft;
-    char *name, *taskstats_genl_name;
+    int req, family_id, msgleft, req_len;
+    char *nlreq_msg, *name, *taskstats_genl_name;
     uint16_t name_len;
     struct nlattr *nla;
     struct nl_msg fam_msg;
@@ -106,8 +104,11 @@ int get_family_id(int conn)
     name_len = strlen(TASKSTATS_GENL_NAME) + 1;
     taskstats_genl_name = strndup(TASKSTATS_GENL_NAME, name_len);
 
-    req = nl_req(conn, GENL_ID_CTRL, CTRL_CMD_GETFAMILY,
-                 CTRL_ATTR_FAMILY_NAME, taskstats_genl_name, name_len);
+    nlreq_msg = build_req(GENL_ID_CTRL, CTRL_CMD_GETFAMILY, 
+                         CTRL_ATTR_FAMILY_NAME, taskstats_genl_name, name_len);
+
+    req_len = GET_REQUEST_LENGTH(nlreq_msg);
+    req = nl_req(conn, nlreq_msg, req_len);
 
     if (req == -1)
         return -1;
@@ -142,17 +143,12 @@ int get_family_id(int conn)
     return family_id;
 }
 
-int nl_req(int conn, uint32_t nl_type, uint32_t gnl_cmd,
+char *build_req(uint32_t nl_type, uint8_t gnl_cmd,
     uint16_t nla_type, void *nla_data, uint16_t nla_len)
 {
-    int pid, bytes_sent;
+    int pid;
     struct nl_msg req;
     struct nlattr *nla;
-    struct sockaddr_nl nladdr;
-    
-    char *buf;
-    ssize_t buflen;
-    socklen_t nladdr_len;
 
     pid = getpid();
     req.nlh.nlmsg_type = nl_type;
@@ -170,12 +166,19 @@ int nl_req(int conn, uint32_t nl_type, uint32_t gnl_cmd,
     memcpy(NLA_DATA(nla), nla_data, nla->nla_len);
     req.nlh.nlmsg_len += NLA_ALIGN(nla->nla_len);
 
+    return (char *) &req;
+}
+            
+int nl_req(int conn, char *buf, int buflen)
+{
+    int bytes_sent;
+    struct sockaddr_nl nladdr;
+    
+    socklen_t nladdr_len;
+
     nladdr_len = sizeof(struct sockaddr_nl);
     memset(&nladdr, 0, nladdr_len);
     nladdr.nl_family = AF_NETLINK;
-
-    buf = (char *) &req;
-    buflen = req.nlh.nlmsg_len;
 
     while ((bytes_sent = sendto(conn, buf, buflen, 0, 
            (struct sockaddr *) &nladdr, nladdr_len)) < buflen) {
@@ -236,7 +239,8 @@ int taskstats_reply(struct nl_msg *reply, proc_t *procs)
 
 void proc_io(proc_t *procs)
 {
-    int req, pid, conn, family_id;
+    char *nlreq_msg;
+    int req, pid, conn, family_id, req_len;
     struct nl_msg io_req;
     conn = create_conn();
 
@@ -249,8 +253,11 @@ void proc_io(proc_t *procs)
         goto error;
 
     pid = procs->pid;
-    req = nl_req(conn, family_id, TASKSTATS_CMD_GET, 
-      TASKSTATS_CMD_ATTR_PID, &pid, sizeof(uint32_t));
+    nlreq_msg = build_req(family_id, TASKSTATS_CMD_GET, 
+                          TASKSTATS_CMD_ATTR_PID, &pid, sizeof(uint32_t));
+
+    req_len = GET_REQUEST_LENGTH(nlreq_msg);
+    req = nl_req(conn, nlreq_msg, req_len);
 
     if (req == -1)
         goto error;
