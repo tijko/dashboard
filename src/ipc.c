c2faebf5335d3a008869b852dfbb94255880b740
Tim Konick <konick781@gmail.com>
2015-05-07 21:35:12 -0400

    move fds to ipc

--- src/ipc.c
+++ src/ipc.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+#include "ipc.h"
 
 
-int ipc(void)
+void current_fds(proc_t *proc)
 {
+    char *path;
+
+    struct dirent *fd_file;
+    DIR *fd_dir;
+
+    path = malloc(sizeof(char) * MAXPROCPATH);
+    if (path == NULL)
+        return;
+
+    snprintf(path, MAXPROCPATH, "/proc/%s/fd/", proc->pidstr);
+   
+    proc->open_fds = 0;
+ 
+    fd_dir = opendir(path);
+    if (fd_dir == NULL) 
+        goto free_path;
+
+    while ((fd_file = readdir(fd_dir))) {
+        if (!isdigit(fd_file->d_name[0]))
+            continue;
+        else
+            proc->open_fds++;
+    }
+
+    closedir(fd_dir);
     
-    return 0;
+    free_path:
+        free(path);
 }
