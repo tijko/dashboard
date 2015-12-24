e97bc17092634f2fec6569afd8098135f34758d3
Tim Konick <konick781@gmail.com>
2015-12-24 15:28:25 -0500

    Untracked commit

--- src/memory.c
+++ src/memory.c
@@ -11,30 +11,30 @@ int total_memory(void)
 {
     char *mempath = "/proc/meminfo";
     char *memory = proc_parser(mempath, "MemTotal");
+
     if (memory != NULL) {
         int value = strtol(memory, NULL, 10);
         free(memory);
         return value;
     }
+
     return -1;
 }
 
 void memory_percentage(proc_t *procs, int totalmem)
 {
     float total_usage;
-    char *percentage;
-    char *path;
 
-    path = construct_path(3, PROC, procs->pidstr, STATUS);
+    char *path = construct_path(3, PROC, procs->pidstr, STATUS);
 
-    percentage = proc_parser(path, VMEM);
+    char *percentage = proc_parser(path, VMEM);
 
     if (percentage != NULL) {
         total_usage = (float) strtol(percentage, NULL, 10);
         free(percentage);
         procs->mempcent = (total_usage / totalmem) * 100;
-    } else {
+    } else 
         procs->mempcent = MINMEM;
-    }
+    
     free(path);
 }
