7ba3a3a67d7eb534a440ba2f540b60e71765249b
Tim Konick <konick781@gmail.com>
2015-12-26 21:16:20 -0500

    mv define keys

--- display/display.h
+++ display/display.h
@@ -42,20 +42,6 @@ enum {
 
 #define DELAY 5
 
-#define KEY_C 99
-#define KEY_D 100
-#define KEY_E 101
-#define KEY_I 105
-#define KEY_M 109
-#define KEY_N 110
-#define KEY_O 111
-#define KEY_P 112
-#define KEY_R 114
-#define KEY_S 115
-#define KEY_T 116
-#define KEY_V 118
-#define KEY_ESCAPE 113
-
 #define FIELDS 15
 
 #define TIME_READ_DEFAULT 0 
