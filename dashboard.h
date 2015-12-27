cf4c1ac18e74c5d3e2f1a43a9ab5d6cef2ee0b09
Tim Konick <konick781@gmail.com>
2015-12-27 06:33:10 -0500

    definitions for mainloop adjustment

---
+++ dashboard.h
@@ -0,0 +1,23 @@
+#ifndef DASHBOARD_H
+#define DASHBOARD_H
+
+#include "src/process/process.h"
+
+
+typedef struct {
+    char *fieldbar;
+    int max_x;
+    int max_y;
+    int prev_x;
+    int prev_y;
+    proc_t *process_list;
+    // build sysinfo
+} board_t;    
+
+void print_usage(void);
+
+char set_sort_option(char *opt);
+
+void dashboard_mainloop(char attr_sort);
+
+#endif
