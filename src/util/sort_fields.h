4aba6bd3dd479dce23c25183244a406b48d399e3
Tim Konick <konick781@gmail.com>
2015-12-26 21:16:02 -0500

    define keys in sort

--- src/util/sort_fields.h
+++ src/util/sort_fields.h
@@ -4,6 +4,20 @@
 #include <stdint.h>
 
 
+#define KEY_C 99
+#define KEY_D 100
+#define KEY_E 101
+#define KEY_I 105
+#define KEY_M 109
+#define KEY_N 110
+#define KEY_O 111
+#define KEY_P 112
+#define KEY_R 114
+#define KEY_S 115
+#define KEY_T 116
+#define KEY_V 118
+#define KEY_ESCAPE 113
+
 proc_t *sort_by_field(proc_t *procs, int field, int nproc);
 
 void cur_fields(proc_t *proc_arr[], proc_t *cur, int proc_index,
