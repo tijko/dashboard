f77524216c4ebb72e6c9f1277cc954103407a6d9
Tim Konick <konick781@gmail.com>
2015-12-27 23:21:45 -0500

    Default file path macros

--- src/util/file_utils.h
+++ src/util/file_utils.h
@@ -3,6 +3,17 @@
 
 #define PATHLEN 32
 
+#define STATUS "/status"
+#define PROC "/proc/"
+#define COMM "/comm"
+#define FD "/fd"
+
+#define UID "Uid"
+#define PTE "VmPTE"
+#define RSS "VmRSS"
+#define VMEM "VmSize"
+#define THRS "Threads"
+
 char *proc_parser(char *file, char *field);
 
 char *construct_path(int pathparts, ...);
