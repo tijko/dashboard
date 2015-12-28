ee3c7e3cfc35747b1ef28d481aad3f076fc8e132
Tim Konick <konick781@gmail.com>
2015-12-27 23:20:15 -0500

    No includes from process.h

--- src/ipc/ipc.c
+++ src/ipc/ipc.c
@@ -7,30 +7,30 @@
 #include "../util/file_utils.h"
 
 
-void current_fds(proc_t *proc)
+int current_fds(char *pidstr)
 {
-    char *path;
-
     struct dirent *fd_file;
-    DIR *fd_dir;
 
-    path = construct_path(3, PROC, proc->pidstr, FD);
+    char *path = construct_path(3, PROC, pidstr, FD);
    
-    proc->open_fds = 0;
+    int open_fds = 0;
  
-    fd_dir = opendir(path);
+    DIR *fd_dir = opendir(path);
     if (fd_dir == NULL) 
         goto free_path;
 
     while ((fd_file = readdir(fd_dir))) {
+
         if (!isdigit(fd_file->d_name[0]))
             continue;
         else
-            proc->open_fds++;
+            open_fds++;
     }
 
     closedir(fd_dir);
     
     free_path:
         free(path);
+
+    return open_fds;
 }
