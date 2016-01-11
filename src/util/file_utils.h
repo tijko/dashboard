8c5a43a89fc8c8ba73bdcd692db139d3ae2c4e6d
Tim Konick <konick781@gmail.com>
2016-01-10 20:56:53 -0500

    Remove construct_path definition

--- src/util/file_utils.h
+++ src/util/file_utils.h
@@ -1,12 +1,21 @@
 #ifndef FILE_UTILS_H
 #define FILE_UTILS_H
 
+#include <string.h>
+
+
 #define PATHLEN 32
 
-#define STATUS "/status"
 #define PROC "/proc/"
-#define COMM "/comm"
-#define FD "/fd"
+
+#define STATUS "/proc/%s/status"
+#define STATUS_LEN strlen(STATUS)
+
+#define COMM "/proc/%s/comm"
+#define COMM_LEN strlen(COMM)
+
+#define FD "/proc/%s/fd"
+#define FD_LEN strlen(FD)
 
 #define UID "Uid"
 #define PTE "VmPTE"
@@ -15,7 +24,5 @@
 #define THRS "Threads"
 
 char *proc_parser(char *file, char *field);
-
-char *construct_path(int pathparts, ...);
 
 #endif
