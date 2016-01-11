608f29331a64b0964852b833994551595cd940e9
Tim Konick <konick781@gmail.com>
2016-01-11 01:13:39 -0500

    Length macros removed

--- src/util/file_utils.h
+++ src/util/file_utils.h
@@ -7,15 +7,12 @@
 #define PATHLEN 32
 
 #define PROC "/proc/"
-
 #define STATUS "/proc/%s/status"
-#define STATUS_LEN strlen(STATUS)
 
 #define COMM "/proc/%s/comm"
 #define COMM_LEN strlen(COMM)
 
 #define FD "/proc/%s/fd"
-#define FD_LEN strlen(FD)
 
 #define UID "Uid"
 #define PTE "VmPTE"
