95ad6d5cd048b460c58fd587dd5682cbc67d2f1f
Tim Konick <konick781@gmail.com>
2016-01-13 14:42:35 -0500

    Updated function definition

--- src/util/sort_fields.h
+++ src/util/sort_fields.h
@@ -25,7 +25,7 @@ proc_t *sort_by_field(proc_t *procs, int field, int nproc);
 void cur_fields(proc_t *proc_arr[], proc_t *cur, int proc_index,
                 int field, uint64_t cmp_fields[]);
 
-proc_t *reorder(proc_t *proc_arr[], proc_t *head, int nproc);
+proc_t *reorder(proc_t *proc_arr[], int nproc);
 
 void init_process_array(proc_t *proc_arr[], proc_t *procs, int nproc);
 
