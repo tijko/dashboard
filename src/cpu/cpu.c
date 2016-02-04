c767778924a60cc0ac4da6c99e9d4bee167226e4
Tim Konick <konick781@gmail.com>
2016-02-03 22:02:46 -0500

    Remove state

--- src/cpu/cpu.c
+++ src/cpu/cpu.c
@@ -2,11 +2,8 @@
 
 #include <stdio.h>
 #include <sched.h>
-#include <ctype.h>
 #include <errno.h>
-#include <string.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <sys/resource.h>
 
 #include "cpu.h"
@@ -38,36 +35,3 @@ uint64_t get_process_ctxt_switches(int pid)
 {
     return task_req(pid, 's');
 }
-
-char *state(char *path)
-{
-    char *field = "State";
-    size_t fieldlen = strlen(field);
-
-    FILE *fp = fopen(path, "r");
-
-    if (fp == NULL) 
-        return NULL;
-            
-    int i;
-    size_t n;
-
-    char *proc_state = NULL, *ln = NULL;
-
-    for (n=0; getline(&ln, &n, fp) !=0;) {
-        *(ln + fieldlen) = '\0';
-        if (!strcmp(ln, field)) {
-            proc_state = malloc(sizeof(char) * STATE);
-            for (i=0; !isspace(ln[i++]); );
-            for (; !isalpha(ln[i++]); );
-            *proc_state = *(ln + i - 1);
-            *(proc_state + 1) = '\0';
-            break;
-        }
-    }
-
-    fclose(fp);
-    free(ln);
-    
-    return proc_state;
-}    
