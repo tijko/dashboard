59242325962fb48b1dad009b24d55cb77cce7753
Tim Konick <konick781@gmail.com>
2016-02-09 17:35:10 -0500

    Update types conversions on sort

--- src/util/sort_fields.c
+++ src/util/sort_fields.c
@@ -8,52 +8,53 @@
 
 proc_t *sort_by_field(proc_t *procs, int field, int nproc)
 {
-    proc_t *proc_arr[nproc + 1];
+    proc_t *ps_array[nproc + 1];
     uint64_t cmp_fields[2] = {0, 0};
 
-    init_process_array(proc_arr, procs, nproc);
+    init_ps_array(ps_array, procs, nproc);
  
     for (int i=0; i < nproc; i++) {
-        proc_t *cur = proc_arr[i + 1];
+        proc_t *cur = ps_array[i + 1];
         int j = i;
-        cur_fields(proc_arr, cur, j, field, cmp_fields);
+        cur_fields(ps_array, cur, j, field, cmp_fields);
         while (j >= 0 && cmp_fields[0] > cmp_fields[1]) {
-            proc_arr[j + 1] = proc_arr[j];
-            proc_arr[j--] = cur;
+            ps_array[j + 1] = ps_array[j];
+            ps_array[j--] = cur;
             if (j >= 0)
-                cur_fields(proc_arr, cur, j, field, cmp_fields);
+                cur_fields(ps_array, cur, j, field, cmp_fields);
         }
 
-        proc_arr[j + 1] = cur;
+        ps_array[j + 1] = cur;
     }
 
-    return reorder(proc_arr, nproc);
+    return reorder(ps_array, nproc);
 }
 
-void cur_fields(proc_t *proc_arr[], proc_t *cur, int proc_index, 
+void cur_fields(proc_t *ps_array[], proc_t *cur, int ps_idx, 
                 int field, uint64_t cmp_fields[])
 {
         switch (field) {
             
             case (KEY_C):
                 cmp_fields[0] = cur->cpuset;
-                cmp_fields[1] = proc_arr[proc_index]->cpuset;
+                cmp_fields[1] = ps_array[ps_idx]->cpuset;
                 break;
 
             case (KEY_D):
                 cmp_fields[0] = cur->open_fds;
-                cmp_fields[1] = proc_arr[proc_index]->open_fds;
+                cmp_fields[1] = ps_array[ps_idx]->open_fds;
                 break;
 
             case (KEY_E):
-                cmp_fields[0] = cur->pte;
-                cmp_fields[1] = proc_arr[proc_index]->pte;
+                cmp_fields[0] = cur->pte == NULL ? 0 : atoi(cur->pte);
+                cmp_fields[1] = ps_array[ps_idx]->pte == NULL ? 
+                                0 : atoi(ps_array[ps_idx]->pte);
                 break;
             
             case (KEY_I):
                 cmp_fields[0] = cur->io_write == NULL ? 0 : atoll(cur->io_write);
-                cmp_fields[1] = proc_arr[proc_index]->io_write == NULL ? 
-                                0 : atoll(proc_arr[proc_index]->io_write);
+                cmp_fields[1] = ps_array[ps_idx]->io_write == NULL ? 
+                                0 : atoll(ps_array[ps_idx]->io_write);
                 break;
 
             /*
@@ -66,66 +67,66 @@ void cur_fields(proc_t *proc_arr[], proc_t *cur, int proc_index,
 
             case (KEY_M):   
                 cmp_fields[0] = cur->mempcent * 100;
-                cmp_fields[1] = proc_arr[proc_index]->mempcent * 100;
+                cmp_fields[1] = ps_array[ps_idx]->mempcent * 100;
                 break;
 
             case (KEY_N):
                 cmp_fields[0] = cur->nice;
-                cmp_fields[1] = proc_arr[proc_index]->nice;
+                cmp_fields[1] = ps_array[ps_idx]->nice;
                 break;
 
             case (KEY_O):
                 cmp_fields[0] = cur->io_read == NULL ? 0 : atoll(cur->io_read);
-                cmp_fields[1] = proc_arr[proc_index]->io_read == NULL ? 
-                                0 : atoll(proc_arr[proc_index]->io_read);
+                cmp_fields[1] = ps_array[ps_idx]->io_read == NULL ? 
+                                0 : atoll(ps_array[ps_idx]->io_read);
                 break;
 
             case (KEY_P):
                 cmp_fields[0] = cur->pid;
-                cmp_fields[1] = proc_arr[proc_index]->pid;
+                cmp_fields[1] = ps_array[ps_idx]->pid;
                 break;
 
             case (KEY_R):
                 cmp_fields[0] = atoi(cur->rss);
-                cmp_fields[1] = atoi(proc_arr[proc_index]->rss);
+                cmp_fields[1] = atoi(ps_array[ps_idx]->rss);
                 break;
             
             case (KEY_S):
                 cmp_fields[0] = cur->invol_sw == NULL ? 0 : atoll(cur->invol_sw);
-                cmp_fields[1] = proc_arr[proc_index]->invol_sw == NULL ?
-                                0 : atoll(proc_arr[proc_index]->invol_sw);
+                cmp_fields[1] = ps_array[ps_idx]->invol_sw == NULL ?
+                                0 : atoll(ps_array[ps_idx]->invol_sw);
                 break;
 
             case (KEY_T):
                 cmp_fields[0] = atoi(cur->thrcnt);
-                cmp_fields[1] = atoi(proc_arr[proc_index]->thrcnt);
+                cmp_fields[1] = atoi(ps_array[ps_idx]->thrcnt);
                 break;
 
             case (KEY_V):
                 cmp_fields[0] = atoi(cur->vmem);
-                cmp_fields[1] = atoi(proc_arr[proc_index]->vmem);
+                cmp_fields[1] = atoi(ps_array[ps_idx]->vmem);
                 break;
         }
 }
 
-proc_t *reorder(proc_t *proc_arr[], int nproc)
+proc_t *reorder(proc_t *ps_array[], int nproc)
 {
     int i = 0;
     for (; i < nproc; i++) {
-        proc_arr[i]->next = proc_arr[i + 1];
-        proc_arr[i + 1]->prev = proc_arr[i];
+        ps_array[i]->next = ps_array[i + 1];
+        ps_array[i + 1]->prev = ps_array[i];
     }
 
-    proc_arr[i]->next = NULL;
-    proc_arr[0]->prev = NULL;
+    ps_array[i]->next = NULL;
+    ps_array[0]->prev = NULL;
 
-    return proc_arr[0];
+    return ps_array[0];
 }
 
-void init_process_array(proc_t *proc_arr[], proc_t *procs, int nproc)
+void init_ps_array(proc_t *ps_array[], proc_t *procs, int nproc)
 {
     for (int i=0; i < nproc + 1; i++) {
-        proc_arr[i] = procs;
+        ps_array[i] = procs;
         procs = procs->next;
     }
 }    
