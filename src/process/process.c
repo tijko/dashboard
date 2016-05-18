69a17bceb7097fd6eca2c28a7284f10c08f09818
Tim <konick781@gmail.com>
2016-05-17 20:39:08 -0400

    Adjust formatting for 64bit long

--- src/process/process.c
+++ src/process/process.c
@@ -79,15 +79,15 @@ void get_process_stats(ps_node *process, sysaux *system)
     if (system->euid == 0) {
         uint64_t io_read = get_process_taskstat_io(process->pid, 'o');
         process->io_read = malloc(sizeof(char) * MAXFIELD);
-        snprintf(process->io_read, MAXFIELD - 1, "%llu", io_read);
+        snprintf(process->io_read, MAXFIELD - 1, "%lu", io_read);
         process->io_read = calculate_size(process->io_read, 0);
         uint64_t io_write = get_process_taskstat_io(process->pid, 'i');
         process->io_write = malloc(sizeof(char) * MAXFIELD);
-        snprintf(process->io_write, MAXFIELD - 1, "%llu", io_write);
+        snprintf(process->io_write, MAXFIELD - 1, "%lu", io_write);
         process->io_write = calculate_size(process->io_write, 0);
         uint64_t invol_sw = get_process_ctxt_switches(process->pid);
         process->invol_sw = malloc(sizeof(char) * MAXFIELD);
-        snprintf(process->invol_sw, MAXFIELD - 1, "%llu", invol_sw);
+        snprintf(process->invol_sw, MAXFIELD - 1, "%lu", invol_sw);
     } else {
         process->io_write = get_user_ps_write(process->pidstr);
         process->io_read = get_user_ps_read(process->pidstr);
